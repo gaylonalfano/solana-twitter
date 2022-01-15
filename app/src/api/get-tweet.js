ef505b09ae99003cebb43754d0f82b5c134d5ffc
gaylonalfano <gaylon.alfano@gmail.com>
2022-01-15 11:32:25 +0800

    Can now click on the timestamp of a single Tweet and fetch that Tweet from the chain. This uses Anchor's workspace.program.account.tweet.fetch(publicKey) API basically to get that since Tweet account data. Neat stuff.

--- app/src/api/get-tweet.js
+++ app/src/api/get-tweet.js
@@ -1,11 +1,12 @@
-export const getTweet = async (publicKey) => {
-    return {
-        publicKey,
-        topic: 'solana',
-        content: 'gm',
-        author_display: 'B1Af..wtRN',
-        created_at: 'Nov 26, 2021 1:03PM',
-        created_ago: 'just now',
-        timestamp: 1637932864,
-    }
+import { Tweet } from "@/models";
+
+// Pass in the Anchor 'workspace' object and an instance of Solana's PublicKey class
+export async function getTweet({ program }, publicKey) {
+  // NOTE Reference /tests/solana-twitter.ts where we do a single fetch on chain
+  const tweetAccount = await program.value.account.tweet.fetch(publicKey);
+  // Return a compatible Tweet structure (model) that works with our frontend
+  // NOTE The program.value.account.tweet returns an object containing
+  // a publicKey and account object, which is what our Tweet model constructor
+  // needs to create a new Tweet object.
+  return new Tweet(publicKey, tweetAccount);
 }
